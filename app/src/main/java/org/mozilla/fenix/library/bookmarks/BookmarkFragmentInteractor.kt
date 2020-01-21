795d9611c37ce06b4febc5a26093796d83454194
codrut.topliceanu <codrut.topliceanu@softvision.ro>
2020-01-21 15:04:56 +0200

    test commit

--- app/src/main/java/org/mozilla/fenix/library/bookmarks/BookmarkFragmentInteractor.kt
+++ app/src/main/java/org/mozilla/fenix/library/bookmarks/BookmarkFragmentInteractor.kt
@@ -26,6 +26,7 @@ class BookmarkFragmentInteractor(
     private val viewModel: BookmarksSharedViewModel,
     private val bookmarksController: BookmarkController,
     private val metrics: MetricController
+//tkfjkdsjfkldjsafds
 ) : BookmarkViewInteractor {
 
     override fun onBookmarksChanged(node: BookmarkNode) {
